f556810ceacb066fe4898656b7a9c092512ca41a
ASurpluso <nie.tao@outlook.com>
2024-11-21 22:17:18 +0800

    Update domains.js

--- domains.js
+++ domains.js
@@ -2,9 +2,10 @@
   { "domain": "yydss.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "yydso.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "yyds0.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
+  { "domain": "yoyoo.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
+{ "domain": "meimei.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
+{ "domain": "mieya.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "yyds0.line.pm", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" },
 { "domain": "yyds0.work.gd", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" },
-{ "domain": "yyds0.run.place", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" },
-  { "domain": "711911.xyz", "registrationDate": "2024-04-16", "expirationDate": "2029-04-15", "system": "SpaceShip", "systemURL": "https://www.spaceship.com/zh" },
-  { "domain": "hello.xyz", "registrationDate": "2024-07-17", "expirationDate": "2025-07-16", "system": "SpaceShip", "systemURL": "https://www.spaceship.com/zh" }
+{ "domain": "yyds0.run.place", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" }
 ]
