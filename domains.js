90fa45da2b456e100c62f851cb29ef3eaafd9e6e
ASurpluso <nie.tao@outlook.com>
2024-11-21 22:34:23 +0800

    Update domains.js

--- domains.js
+++ domains.js
@@ -1,17 +1,17 @@
 [
-  { "domain": "yyds0.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
-  { "domain": "yydso.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
-  { "domain": "yydsoo.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
-  { "domain": "nienie.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
-  { "domain": "cosmos.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
-  { "domain": "cosmic.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "yyds0.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "yydso.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "yydsoo.us.kg", "registrationDate": "2024-11-1", "expirationDate": "2025-11-1", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "nienie.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "cosmos.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
+  { "domain": "cosmic.us.kg", "registrationDate": "2024-11-2", "expirationDate": "2025-11-2", "system": "US.KG(180天内renew)", "systemURL": "https://register.us.kg/panel/main" },
 { "domain": "yydss.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "yydso.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "yyds0.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
   { "domain": "yoyoo.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "meimei.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
 { "domain": "mieya.ggff.net", "registrationDate": "2024-11-15", "expirationDate": "2025-11-15", "system": "l53.net", "systemURL": "https://customer.l53.net" },
-{ "domain": "yyds0.line.pm", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" },
-{ "domain": "yyds0.work.gd", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" },
-{ "domain": "yyds0.run.place", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit.com", "systemURL": "https://dnsexit.com/" }
+{ "domain": "yyds0.line.pm", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit(30天内renew)", "systemURL": "https://dnsexit.com/" },
+{ "domain": "yyds0.work.gd", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit(30天内renew)", "systemURL": "https://dnsexit.com/" },
+{ "domain": "yyds0.run.place", "registrationDate": "2024-11-02", "expirationDate": "2025-11-02", "system": "dnsexit(30天内renew)", "systemURL": "https://dnsexit.com/" }
 ]
